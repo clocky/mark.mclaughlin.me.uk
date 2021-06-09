9329237536c428c16fad798ab06b9a36b8e271a8
Mark McLaughlin <mark@mclaughlin.me.uk>
2021-06-08 18:50:32 -0700

    Passthrough CSS

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
-module.exports = eleventyConfig => {
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("src/et-book");
+  eleventyConfig.addPassthroughCopy("src/img");
+  eleventyConfig.addPassthroughCopy("src/tufte.css");
+  eleventyConfig.addPassthroughCopy("src/tufte.min.css");
   return {
     dir: {
-      input: 'src',
-      output: 'dist'
-    }
-  }
+      input: "src",
+      output: "dist",
+    },
+  };
 };
