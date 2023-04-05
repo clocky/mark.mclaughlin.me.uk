547d912de6fe5385ec19b623ecfaed142e4829e1
Mark McLaughlin <mark@mclaughlin.me.uk>
2023-04-05 19:45:33 +0000

    chore: Quotes

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,31 @@
-module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("src/et-book");
-  eleventyConfig.addPassthroughCopy("src/img");
-  eleventyConfig.addPassthroughCopy("src/tufte.css");
-  eleventyConfig.addPassthroughCopy("src/tufte.min.css");
+const EleventyVitePlugin = require("@11ty/eleventy-plugin-vite");
+const { ViteImageOptimizer } = require("vite-plugin-image-optimizer");
 
+module.exports = function (eleventyConfig) {
+  // Copy over Tufte fonts, and styles
+  eleventyConfig.addPassthroughCopy("src/et-book");
+  eleventyConfig.addPassthroughCopy("src/tufte.css");
+
+  // Copy over images
+  eleventyConfig.addPassthroughCopy("src/img");
+
+  // Make global filters accessible to Pug templates
   global.filters = eleventyConfig.javascriptFunctions;
-  eleventyConfig.setPugOptions({ 
-          globals: ['filters']
+  eleventyConfig.setPugOptions({
+    globals: ["filters"],
   });
+
+  // Instanciate the Vite plugin
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      plugins: [
+        ViteImageOptimizer({
+          /* pass your config */
+        }),
+      ],
+    },
+  });
+
   return {
     dir: {
       input: "src",
