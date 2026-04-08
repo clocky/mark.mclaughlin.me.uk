81a29fda1db7bd7b1055a0f605c7fa43e39b966f
Mark McLaughlin <mark@mclaughlin.me.uk>
2026-04-08 10:48:17 -0700

    chore: add eleventy vite configuration

---
+++ eleventy.config.mjs
@@ -0,0 +1,42 @@
+import EleventyVitePlugin from "@11ty/eleventy-plugin-vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default function (eleventyConfig) {
+
+  // Copy over Tufte fonts, and styles
+  eleventyConfig.addPassthroughCopy({ "src/assets/et-book": "css/et-book" });
+  eleventyConfig.addPassthroughCopy({ "src/assets/css/": "css" });
+
+  // Copy over images
+  eleventyConfig.addPassthroughCopy({ "src/assets/images": "img" });
+
+  // Make global filters accessible to Pug templates
+  global.filters = eleventyConfig.javascriptFunctions;
+  eleventyConfig.setPugOptions({
+    globals: ["filters"],
+  });
+
+  // Instantiate the Vite plugin.
+  eleventyConfig.addPlugin(EleventyVitePlugin, {
+    viteOptions: {
+      plugins: [
+        ViteImageOptimizer({
+          jpg: {
+            quality: 80,
+          },
+          png: {
+            quality: 25,
+            dither: 0.5,
+          },
+        }),
+      ],
+    },
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+}
